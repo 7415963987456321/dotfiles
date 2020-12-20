568d1ef6f7912bffda17ea0f2087d6a08c6e2a12
7415963987456321 <hrs70@hi.is>
2020-12-20 01:27:53 +0000

    Fix fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,13 +9,16 @@
 
 ### Fish greeter
 function fish_greeting
-	figlet \: \( \) \{ \: \| \: \& \} \; \: 
-	neofetch --stdout 
+    figlet \: \( \) \{ \: \| \: \& \} \; \:
+    #neofetch --stdout
 end
 
+# NNN settings (clean this up)
 export NNN_TMPFILE="/tmp/nnn"
 export NNN_PLUG='o:fzopen;d:diffs;m:nmount;n:notes;v:imgviu;p:mpv'
-
+# export NNN_FCOLORS='c1e2272e006033f7c6d6abc4'
+export NNN_COLORS='6537'
+export NNN_FCOLORS="a5a0d677fb2121f7c6d6abc4"
 function n --description 'support nnn quit and change directory'
     # Block nesting of nnn in subshells
     if [ (expr $NNNLVL + 0) -ge 1 ]
@@ -74,7 +77,8 @@ set -xU LESS_TERMCAP_us (printf "\e[01;32m")
 setxkbmap is
 
 ### ALIAS
-#alias n=nnn
+# alias n=nnn
+alias nh=nohup
 alias mk=touch
 alias config='/usr/bin/git --git-dir=$HOME/.config/ --work-tree=$HOME'
 alias Syu='sudo pacman -Syu'
@@ -89,13 +93,46 @@ alias tm='tmux'
 alias m='man'
 alias mu='mupdf'
 alias gt='gotop'
-alias wttr='curl -s wttr.in'
+alias wttr='curl -s \'wttr.in/Reykjavik?M\''
 alias clone='~/.config/fish/scripts/clone.sh'
+alias yt='~/.config/fish/scripts/yt.sh'
+alias journalctl='journalctl -xe'
+alias nb='newsboat'
+
+#pastebin
+alias pb='curl -F \'file=@-\' https://0x0.st'
+
+# Coloring
+alias ip='ip       --color=auto'
+alias grep='grep   --color=auto'
+alias dmesg='dmesg --color=auto'
+
+set -xU LESS_TERMCAP_md (printf "\e[01;31m")
+set -xU LESS_TERMCAP_me (printf "\e[0m")
+set -xU LESS_TERMCAP_se (printf "\e[0m")
+set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")
+set -xU LESS_TERMCAP_ue (printf "\e[0m")
+set -xU LESS_TERMCAP_us (printf "\e[01;32m")
+
 # Git stöff
 alias ga='git add'
 alias gc='git checkout'
 alias gcm='git checkout master'
 alias gb='git branch'
-alias gs='git stash'
+alias gs='git status'
+alias gl='git log'
+alias gd='git diff'
 
+# Hacky media 
 alias mus='n /run/media/keli/TunaFish/Music'
+
+# Networking
+export KELI_SERVER=keli@192.168.1.102
+export SSH_PORT=1337
+alias server='ssh -p $SSH_PORT $KELI_SERVER'
+alias whoami="whoami && curl ident.me"
+
+# Goddamnit fish-shell, just support IGNOREEOF
+bind \cd\cd\cd delete-or-exit
+
+
